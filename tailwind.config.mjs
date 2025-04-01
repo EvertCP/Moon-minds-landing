1fe7007df3af3315a75109e80c4a2721446b8b00
Evert <evert_carpo@hotmail.com>
2025-04-01 13:11:38 -0600

    Agregar funcionalidades

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,14 @@
+// tailwind.config.mjs
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {
-			backgroundImage: {
-				'hero-pattern': "url('/src/assets/portada-moon-minds.png')",
-				'footer-texture': "url('/img/footer-texture.png')",
-			},
+	  extend: {
+		backgroundImage: {
+		  'hero-pattern': "url('/src/assets/portada-moon-minds.png')",
+		  'footer-texture': "url('/img/footer-texture.png')",
 		},
+	  },
 	},
-	plugins: [],
-}
+	plugins: [require('daisyui')],
+  }
